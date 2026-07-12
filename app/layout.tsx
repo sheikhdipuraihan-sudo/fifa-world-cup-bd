92077246598f3ed91c0e6ed3253ee08ad623ae52
sheikhdipuraihan-sudo <sheikhdipuraihan@gmail.com>
2026-07-12 15:23:26 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,8 @@ import type { Metadata, Viewport } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'GoalZone Live',
+  description: 'Created by Raihan',
   icons: {
     icon: [
       {
